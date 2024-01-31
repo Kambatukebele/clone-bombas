437f9fdba9e9330fa483fa5ac50d82f4bbd6d8d8
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-01-31 19:16:25 +0100

    working on index.html 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       colors: {
         "announcement-blue": "#2E4366",
       },
+      backgroundImage: {
+        underwear: "url('/images/1-Underwear-Mobile.webp')",
+        socks: "url('/images/3_Socks-Mobile.webp')",
+        slippers: "url('/images/4_Slippers-Mobile.webp')",
+        tShirts: "url('/images/2-Shirts-Mobile.webp')",
+      },
     },
   },
   plugins: [],
