4c8e577a311e7073a6f3b31a3ec6a3b9b212eb36
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2024-01-31 11:09:16 +0100

    working on index.html 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      colors: {
+        "announcement-blue": "#2E4366",
+      },
     },
   },
   plugins: [],
